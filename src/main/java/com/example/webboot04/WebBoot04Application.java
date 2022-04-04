fd254364b8104cd34b8c9c7072b0c82350aba4d3
fuyongjun <1335809483@qq.com>
2022-04-04 19:38:28 +0800

    xs

---
+++ src/main/java/com/example/webboot04/WebBoot04Application.java
@@ -0,0 +1,13 @@
+package com.example.webboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBoot04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBoot04Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/webboot04/controller/LoginController.java
@@ -0,0 +1,16 @@
+/*
+@ClassName LoginController
+@Description TODO
+@Author SubUp
+@Date   2022/4/4
+@Version 1.0
+*/
+
+package com.example.webboot04.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+}
